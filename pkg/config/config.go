84eeacfbfdd566fbdcf5b9700725a8a61814a078
mohsen <mohsen.mkh88@gmail.com>
2020-09-26 00:13:15 +0200

    add config package

---
+++ pkg/config/config.go
@@ -0,0 +1,132 @@
+package config
+
+import (
+	"sync"
+)
+
+var confWatch = configHolder{}
+
+type configHolder struct {
+	lock      sync.RWMutex
+	confItems []confItem
+}
+
+type confItem struct {
+	key      string
+	ref      interface{}
+	defValue interface{}
+}
+
+func (cc *configHolder) addRef(key string, ref interface{}, defValue interface{}) {
+	cc.lock.Lock()
+	defer cc.lock.Unlock()
+	cc.confItems = append(cc.confItems, confItem{key: key, ref: ref, defValue: defValue})
+}
+
+func RegisterString(key, defValue string) String { return confWatch.RegisterString(key, defValue) }
+func (cc *configHolder) RegisterString(key, defValue string) String {
+	var v = defValue
+	cc.addRef(key, &v, defValue)
+	return stringHolder{value: &v}
+}
+
+func RegisterInt(key string, defValue int) Int { return confWatch.RegisterInt(key, defValue) }
+func (cc *configHolder) RegisterInt(key string, defValue int) Int {
+	var v = int64(defValue)
+	cc.addRef(key, &v, defValue)
+	return intHolder{value: &v}
+}
+
+func RegisterInt64(key string, defValue int64) Int { return confWatch.RegisterInt64(key, defValue) }
+func (cc *configHolder) RegisterInt64(key string, defValue int64) Int {
+	var v = defValue
+	cc.addRef(key, &v, defValue)
+	return intHolder{value: &v}
+}
+
+func RegisterFloat32(key string, defValue float32) Float {
+	return confWatch.RegisterFloat32(key, defValue)
+}
+func (cc *configHolder) RegisterFloat32(key string, defValue float32) Float {
+	var v = float64(defValue)
+	cc.addRef(key, &v, defValue)
+	return floatHolder{value: &v}
+}
+
+func RegisterFloat64(key string, defValue float64) Float {
+	return confWatch.RegisterFloat64(key, defValue)
+}
+func (cc *configHolder) RegisterFloat64(key string, defValue float64) Float {
+	var v = defValue
+	cc.addRef(key, &v, defValue)
+	return floatHolder{value: &v}
+}
+
+func RegisterBool(key string, defValue bool) Bool { return confWatch.RegisterBool(key, defValue) }
+func (cc *configHolder) RegisterBool(key string, defValue bool) Bool {
+	var v = defValue
+	cc.addRef(key, &v, defValue)
+	return boolHolder{value: &v}
+}
+
+func (cc *configHolder) handleChange() error {
+	cc.lock.RLock()
+	defer cc.lock.RUnlock()
+
+	for _, configItem := range cc.confItems {
+		switch configItem.defValue.(type) {
+		case string:
+
+			v, err := getViperString(configItem.key, configItem.defValue)
+			if err != nil {
+				return err
+			}
+			t := configItem.ref.(*string)
+			*t = v
+		case int:
+			v, err := getViperInt64(configItem.key, configItem.defValue)
+			if err != nil {
+				return err
+			}
+			t := configItem.ref.(*int64)
+			*t = v
+		case int64:
+			v, err := getViperInt64(configItem.key, configItem.defValue)
+			if err != nil {
+				return err
+			}
+			t := configItem.ref.(*int64)
+			*t = v
+		case float32:
+			v, err := getViperFloat32(configItem.key, configItem.defValue)
+			if err != nil {
+				return err
+			}
+			t := configItem.ref.(*float32)
+			*t = v
+		case float64:
+			v, err := getViperFloat64(configItem.key, configItem.defValue)
+			if err != nil {
+				return err
+			}
+			t := configItem.ref.(*float64)
+			*t = v
+		case bool:
+			v, err := getViperBool(configItem.key, configItem.defValue)
+			if err != nil {
+				return err
+			}
+			t := configItem.ref.(*bool)
+			*t = v
+		}
+	}
+	return nil
+}
+
+func Load() error {
+	return confWatch.handleChange()
+}
+
+func Init(confName, ext, appName string) error {
+	return initViper(confName, ext, appName, confWatch.handleChange)
+}
---
+++ pkg/config/viperwrapper.go
@@ -0,0 +1,107 @@
+package config
+
+import (
+	"fmt"
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
+
+func getViperString(key string, defaultValue interface{}) (string, error) {
+	v := viper.GetString(key)
+	if v == "" {
+		s, ok := defaultValue.(string)
+		if ok {
+			return s, nil
+		}
+		return "", fmt.Errorf("convert key %s to string is failed", key)
+	}
+	return v, nil
+}
+
+func getViperInt(key string, defaultValue interface{}) (int, error) {
+	v := viper.GetInt(key)
+	if v == 0 {
+		s, ok := defaultValue.(int)
+		if ok {
+			return s, nil
+		}
+		return 0, fmt.Errorf("convert key %s to int is failed", key)
+	}
+	return v, nil
+}
+
+func getViperInt64(key string, defaultValue interface{}) (int64, error) {
+	v := viper.GetInt64(key)
+	if v == 0 {
+		s, ok := defaultValue.(int64)
+		if ok {
+			return s, nil
+		}
+		return 0, fmt.Errorf("convert key %s to int64 is failed", key)
+	}
+	return v, nil
+}
+
+func getViperFloat32(key string, defaultValue interface{}) (float32, error) {
+	v := viper.GetFloat64(key)
+	if v == 0 {
+		s, ok := defaultValue.(float64)
+		if ok {
+			return float32(s), nil
+		}
+		return 0, fmt.Errorf("convert key %s to int64 is failed", key)
+	}
+	return float32(v), nil
+}
+
+func getViperFloat64(key string, defaultValue interface{}) (float64, error) {
+	v := viper.GetFloat64(key)
+	if v == 0 {
+		s, ok := defaultValue.(float64)
+		if ok {
+			return s, nil
+		}
+		return 0, fmt.Errorf("convert key %s to int64 is failed", key)
+	}
+	return v, nil
+}
+
+func getViperBool(key string, defaultValue interface{}) (bool, error) {
+	v := viper.GetBool(key)
+	if v == false {
+		s, ok := defaultValue.(bool)
+		if ok {
+			return s, nil
+		}
+		return false, fmt.Errorf("convert key %s to bool is failed", key)
+	}
+	return v, nil
+}
+
+func initViper(confName, ext, appName string, onChange func() error) error {
+	viper.SetConfigName(confName)                          // name of config file (without extension)
+	viper.SetConfigType(ext)                               // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath(fmt.Sprintf("/etc/%s", appName))   // path to look for the config file in
+	viper.AddConfigPath(fmt.Sprintf("$HOME/.%s", appName)) // call multiple times to add many search paths
+	viper.AddConfigPath(".")                               // optionally look for config in the working directory
+	err := viper.ReadInConfig()                            // Find and read the config file
+	if err != nil {                                        // Handle errors reading the config file
+		return fmt.Errorf("Fatal error config file: %s \n", err)
+	}
+
+	err = onChange()
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		viper.WatchConfig()
+		viper.OnConfigChange(func(e fsnotify.Event) {
+			err := onChange()
+			if err != nil {
+				panic(err)
+			}
+		})
+	}()
+	return err
+}
