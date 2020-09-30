30ca3c5ee951ae778dd6303c182671c75d52c87f
mohsen <mohsen.mkh88@gmail.com>
2020-09-30 14:59:51 +0200

    auth middlware

---
+++ services/users/auth/middlewares.go
@@ -0,0 +1,65 @@
+package auth
+
+import (
+	"context"
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	"github.com/mirzakhany/pm/pkg/kv"
+	"github.com/mirzakhany/pm/pkg/session"
+	users "github.com/mirzakhany/pm/services/users/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/mirzakhany/pm/pkg/grpcgw"
+	"google.golang.org/grpc"
+)
+
+type contextKey int
+
+const (
+	resourceKey contextKey = iota
+	userKey
+	tokenKey
+	fullMethodKey
+)
+
+func streamExtractor(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+	return handler(srv, ss)
+}
+
+func unaryExtractor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+	res, ok := kv.Memory().Get(info.FullMethod)
+	if !ok {
+		ctx = context.WithValue(ctx, resourceKey, res)
+	}
+	ctx = context.WithValue(ctx, fullMethodKey, info.FullMethod)
+	return handler(ctx, req)
+}
+
+func authHandler(ctx context.Context) (context.Context, error) {
+	r := ctx.Value(resourceKey)
+	if r == nil { // No user requested here
+		return ctx, nil
+	}
+	token, err := grpc_auth.AuthFromMD(ctx, "bearer")
+	if err != nil {
+		return ctx, status.Errorf(codes.InvalidArgument, "invalid token format")
+	}
+	var user *users.User
+	err = session.Get(token, &user)
+	if err != nil {
+		return ctx, status.Errorf(codes.Unauthenticated, "invalid token")
+	}
+	return context.WithValue(context.WithValue(ctx, user, userKey), tokenKey, token), nil
+}
+
+func init() {
+	grpcgw.RegisterInterceptors(grpcgw.Interceptor{
+		Unary:  unaryExtractor,
+		Stream: streamExtractor,
+	})
+
+	grpcgw.RegisterInterceptors(grpcgw.Interceptor{
+		Unary:  grpc_auth.UnaryServerInterceptor(authHandler),
+		Stream: grpc_auth.StreamServerInterceptor(authHandler),
+	})
+}
