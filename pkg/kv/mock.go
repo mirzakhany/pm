24ff67c4a5697b473fa36ca618ef78dbad752b85
mohsen <mohsen.mkh88@gmail.com>
2020-09-30 13:53:57 +0200

    test key value storage

---
+++ pkg/kv/mock.go
@@ -0,0 +1,22 @@
+package kv
+
+import (
+	"context"
+	"github.com/alicebob/miniredis/v2"
+	"github.com/mirzakhany/pm/pkg/config"
+	"strconv"
+)
+
+var redisServer *miniredis.Miniredis
+
+func InitMock(ctx context.Context) (*Client, error) {
+	var err error
+	redisServer, err = miniredis.Run()
+	if err != nil {
+		panic(err)
+	}
+	serverPort, _ := strconv.Atoi(redisServer.Port())
+	host = config.RegisterStringMock("redis.host", redisServer.Host())
+	port = config.RegisterIntMock("redis.port", serverPort)
+	return Init(ctx)
+}
